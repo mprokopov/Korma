e592a1214869ba5c929830a17380507c806d0156
Max Prokopov <mprokopov@gmail.com>
2017-01-02 07:45:59 +0200

    for clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject korma "0.4.3"
+(defproject mprokopov/korma "0.4.4"
   :description "Tasty SQL for Clojure"
   :url "http://github.com/korma/Korma"
   :mailing-list {:name "Korma Google Group"
